e074ce7da88858f596c8841c865e898dd3f94e0c
KodaE <koda4739@gmail.com>
2023-09-13 23:14:51 +1000

    Added placeholder code

---
+++ Reach_Control.py
@@ -0,0 +1,12 @@
+from bplprotocol import BPLProtocol, PacketID
+import time
+import serial
+
+class Reach_Control_Class:
+    def __init__(self) -> None:
+        pass
+
+# Runs code if Reach_Control.py is run directly
+if __name__ == "__main__":
+    ReachComObj = Reach_Control_Class()
+
