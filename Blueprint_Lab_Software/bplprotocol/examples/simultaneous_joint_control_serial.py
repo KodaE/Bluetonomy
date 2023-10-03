88b6ebf017f90192f28f7d3313060bf3032e939c
dugarte4 <daniel.m.ugarte4@gmail.com>
2023-10-03 13:43:37 +1100

    Position Feedback Test & Robot Coordinate Test ProgressTest

--- Blueprint_Lab_Software/bplprotocol/examples/simultaneous_joint_control_serial.py
+++ Blueprint_Lab_Software/bplprotocol/examples/simultaneous_joint_control_serial.py
@@ -1,4 +1,4 @@
-from bplprotocol import BPLProtocol, PacketID
+from bplprotocol import BPLProtocol, PacketID, PacketReader
 
 import time
 import serial
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     # Desired position from end effector to base A -> G
     #desired_positions = [10.0, 0.5, 1.5707, 1.5707, 1.5707, 2.8, 3.1415]
     q0 = ((350/180.0)*3.14)
-    print(q0)
-    desired_positions = [10.0, 0.0, 0.0, 1.5707,0]
+    # print(q0)
+    desired_positions = [10.0, 0.0, 1.0, 1.5707,1.2]
 
 
    # desired_positions = [0,0,0,0,0,0,0]
@@ -28,3 +28,44 @@ if __name__ == '__main__':
     # Send joints to desired_positions
     serial_port.write(packets)
 
+    request_packet = b''
+
+    
+    device_ids = [0x01, 0x02, 0x03, 0x04, 0x05]
+
+    frequency = 5
+
+    packet_reader = PacketReader()
+
+    # Request packets can be concatenated
+    for device_id in device_ids:
+        request_packet += BPLProtocol.encode_packet(device_id, PacketID.REQUEST, bytes(PacketID.POSITION))
+        while True:
+
+            # Send request packet
+            serial_port.write(request_packet)
+
+            position_responses = {}
+            # Read request packets
+            
+            start_time = time.time()
+            while time.time() < start_time + 1/frequency:
+                time.sleep(0.01)
+                try:
+                    read_data = serial_port.read()
+                except BaseException:
+                    
+                    read_data = b''
+                if read_data != b'':
+                    
+                    packets = packet_reader.receive_bytes(read_data)
+                    if packets:
+                        
+                        for packet in packets:
+                            print(packet)
+                            read_device_id, read_packet_id, data_bytes = packet
+                            if read_device_id in device_ids and read_packet_id == PacketID.POSITION:
+                                position = BPLProtocol.decode_floats(data_bytes)[0]
+
+                                position_responses[read_device_id] = position
+            print(f"Positions {position_responses}")
