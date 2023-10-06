c2bc75582466fadcea31013e1a204615b1e6d2bb
unknown <100dayscode.minh@gmail.com>
2023-10-06 13:31:48 +1100

    Add other feedback data and structured into a list

--- Reach_Control.py
+++ Reach_Control.py
@@ -24,12 +24,44 @@ class Reach_Control_Class:
 
         self.serial_port.write(self.packets)
 
-    def kinematics_pos(self):
-        packet_reader = PacketReader()
-        self.serial_port.write(BPLProtocol.encode_packet(self.ra5_base_id, PacketID.REQUEST, bytes([PacketID.KM_END_POS])))
-        start_time = time.time()
+    # def kinematics_pos(self):
+    #     packet_reader = PacketReader()
+    #     self.serial_port.write(BPLProtocol.encode_packet(self.ra5_base_id, PacketID.REQUEST, bytes([PacketID.KM_END_POS])))
+    #     start_time = time.time()
 
+    #     position = None
+    #     while True:
+    #         time.sleep(0.0001)
+    #         try:
+    #             read_data = self.serial_port.read()
+    #         except BaseException:
+    #             read_data = b''
+    #         if read_data != b'':
+    #             packets = packet_reader.receive_bytes(read_data)
+    #             if packets:
+    #                 for packet in packets:
+    #                     read_device_id, read_packet_id, data_bytes = packet
+    #                     if read_device_id == self.ra5_base_id and read_packet_id == PacketID.KM_END_POS:
+    #                         # Decode floats, because position is reported in floats
+    #                         position = BPLProtocol.decode_floats(data_bytes)[:3]
+    #                         return position             
+                                                
+    #                 if position is not None:
+    #                     break
+
+    #     # Timeout if no response is seen from the device.
+    #         if time.time() - start_time > self.request_timeout:
+    #             print("Request for Position timed out")
+    #             break
+
+    def base_id_feedback_data(self):
+        packet_reader = PacketReader()
+        self.serial_port.write(BPLProtocol.encode_packet(self.ra5_base_id, PacketID.REQUEST, bytes([PacketID.KM_END_POS, PacketID.VOLTAGE, PacketID.TEMPERATURE])))
+        start_time = time.time()
         position = None
+        voltage = None
+        temp = None
+        d = dict()
         while True:
             time.sleep(0.0001)
             try:
@@ -41,18 +73,30 @@ class Reach_Control_Class:
                 if packets:
                     for packet in packets:
                         read_device_id, read_packet_id, data_bytes = packet
-                        if read_device_id == self.ra5_base_id and read_packet_id == PacketID.KM_END_POS:
-                            # Decode floats, because position is reported in floats
-                            position = BPLProtocol.decode_floats(data_bytes)[:3]
-                            return position             
-                                                
-                    if position is not None:
+                        if read_device_id == self.ra5_base_id and read_packet_id in [PacketID.KM_END_POS, PacketID.VOLTAGE, PacketID.TEMPERATURE]:
+                            if read_packet_id == PacketID.KM_END_POS:
+                                position = BPLProtocol.decode_floats(data_bytes)[:3]
+                            elif read_packet_id == PacketID.VOLTAGE:
+                                voltage = BPLProtocol.decode_floats(data_bytes)
+                            elif read_packet_id == PacketID.TEMPERATURE:
+                                temp = BPLProtocol.decode_floats(data_bytes)
+
+
+                    if position is not None and voltage is not None and temp is not None:
                         break
 
-        # Timeout if no response is seen from the device.
+            # Timeout if no response is seen from the device.
             if time.time() - start_time > self.request_timeout:
-                print("Request for Position timed out")
+                print("Request for Position and Velocity Timed out")
                 break
+
+        if position is not None and voltage is not None and temp is not None:
+            d["km-pos"] = position
+            d["voltage"] = voltage
+            d["temp"] = temp
+            return d
+
+
 
       
 if __name__ == "__main__":
