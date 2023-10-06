a09218d305f2d6d8e0fc793664e821e7366db9fc
unknown <100dayscode.minh@gmail.com>
2023-10-06 16:59:36 +1100

    Implement reach control function to get joint data

--- Reach_Control.py
+++ Reach_Control.py
@@ -8,51 +8,20 @@ class Reach_Control_Class:
     def __init__(self,serial_port_name): 
         self.serial_port_name = serial_port_name
         self.packets =b''
+        self.device_id = [0x01, 0x02, 0x03, 0x04, 0x05]
         self.serial_port = serial.Serial(self.serial_port_name, baudrate=115200, parity=serial.PARITY_NONE,
                                 stopbits=serial.STOPBITS_ONE, timeout=0)
         self.ra5_base_id = 0x05
-        self.request_timeout =  0.5
+        self.request_timeout =  1
       
-
     def move_arm_to_pos(self, desired_positions):
-        device_id = ()
-    
-
-        for index, position in enumerate(desired_positions):
-            device_id = index + 1
-            self.packets += BPLProtocol.encode_packet(device_id, PacketID.POSITION, BPLProtocol.encode_floats([position]))
-
+        n = 0
+        for position in desired_positions:
+            self.packets += BPLProtocol.encode_packet(self.device_id[n], PacketID.POSITION, BPLProtocol.encode_floats([position]))
+            n += 1
         self.serial_port.write(self.packets)
+        print(self.packets)
 
-    # def kinematics_pos(self):
-    #     packet_reader = PacketReader()
-    #     self.serial_port.write(BPLProtocol.encode_packet(self.ra5_base_id, PacketID.REQUEST, bytes([PacketID.KM_END_POS])))
-    #     start_time = time.time()
-
-    #     position = None
-    #     while True:
-    #         time.sleep(0.0001)
-    #         try:
-    #             read_data = self.serial_port.read()
-    #         except BaseException:
-    #             read_data = b''
-    #         if read_data != b'':
-    #             packets = packet_reader.receive_bytes(read_data)
-    #             if packets:
-    #                 for packet in packets:
-    #                     read_device_id, read_packet_id, data_bytes = packet
-    #                     if read_device_id == self.ra5_base_id and read_packet_id == PacketID.KM_END_POS:
-    #                         # Decode floats, because position is reported in floats
-    #                         position = BPLProtocol.decode_floats(data_bytes)[:3]
-    #                         return position             
-                                                
-    #                 if position is not None:
-    #                     break
-
-    #     # Timeout if no response is seen from the device.
-    #         if time.time() - start_time > self.request_timeout:
-    #             print("Request for Position timed out")
-    #             break
 
     def base_id_feedback_data(self):
         packet_reader = PacketReader()
@@ -95,17 +64,47 @@ class Reach_Control_Class:
             d["voltage"] = voltage
             d["temp"] = temp
             return d
-
-
-
-      
-if __name__ == "__main__":
-    RA5_control = Reach_Control_Class(serial_port_name="COM6")
-
-    result = RA5_control.base_id_data
-
-    print(result)
         
+    
+    def joint_id_feedback_data(self):
+    
+      
+        request_timeout = 1
+        d = dict()
+          
+        for device_id in self.device_id:
+          
+            position = None
+            packet_reader = PacketReader()  
+            self.serial_port.write(BPLProtocol.encode_packet(device_id, PacketID.REQUEST, bytes([PacketID.POSITION]))) 
+            start_time = time.time()
+            while True:
+                time.sleep(0.0001)
+                try:
+                    read_data = self.serial_port.read()
+                except BaseException:
+                    read_data = b''
+                if read_data != b'':
+                    packets = packet_reader.receive_bytes(read_data)
+                    if packets:
+                        for packet in packets:
+                            read_device_id, read_packet_id, data_bytes = packet
+                            if read_device_id == device_id and read_packet_id == PacketID.POSITION:
+                                # Decode floats, because position is reported in floats
+                                position = BPLProtocol.decode_floats(data_bytes)
+                                d[device_id] = position
+                                                          
+                        if position is not None:
+                            break
+
+                # Timeout if no response is seen from the device.
+                if time.time() - start_time > request_timeout:
+                    print("Request for Position timed out")
+                    break
+                
+        return d           
+
+ 
     
 
     
