52f14b6e6d34480c93a32bd0b17b2dccad19a998
Stuartuts <Stuart.Macdonald@student.uts.edu.au>
2023-10-09 03:27:33 +1100

    Update to Kinematics Class (Move to location and back with proof through simulation)

--- Virtual_WorkSpace/Kinematics_Class.py
+++ Virtual_WorkSpace/Kinematics_Class.py
@@ -20,25 +20,73 @@ import serial
 
 class Kinematics:
 
-    def __init__(self, Coordinates):
+    def __init__(self, Coordinates, COMPORT):
+        self.comport = COMPORT
+        #self.serial_port = serial.Serial(self.comport, baudrate=115200, parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE, timeout=0)
         self.coordinates = np.array(Coordinates)
         self.ModelRobot()
-        self.CalculatePosition()
+        self.CalculateandMove()
+        
 
     def ModelRobot(self):
         self.ThetaA = tanh(145.3/40)
-        Link0 = DHLink(d= 46.2, a= 20, alpha= pi/2, qlim= [radians(-175),radians(175)]) # Base Link
-        Link1 = DHLink(d= 0, a= 150.71, alpha= pi, qlim= [-radians(100),radians(100)]) 
-        Link2 = DHLink(d= 0, a= 20, alpha= -pi/2, qlim= [-radians(100),radians(100)])
-        Link3 = DHLink(d= -180, a= 0, alpha= pi/2, qlim= [radians(-175),radians(175)])
-        Link4 = DHLink(theta= -pi/2 ,d= 0, a= 0, alpha= 0, qlim= [0, radians(90)]) # Grabber
+        Link0 = DHLink(d= 0.0462, a= 0.020, alpha= pi/2, qlim= [radians(0)+pi,radians(350)+pi]) # Base Link
+        Link1 = DHLink(d= 0, a= 0.15071, alpha= pi, qlim= [1.5707-self.ThetaA,radians(200)-self.ThetaA]) 
+        Link2 = DHLink(d= 0, a= 0.020, alpha= -pi/2, qlim= [radians(0)-self.ThetaA,radians(200)-self.ThetaA])
+        Link3 = DHLink(d= -0.180, a= 0, alpha= pi/2, qlim= [radians(0)+pi/2,radians(350)+pi/2])
+        Link4 = DHLink(d= 0, a= 0, alpha= 0, qlim= [radians(0), radians(90)]) # Grabber
         self.ReachAlpha5 = DHRobot([Link0 , Link1, Link2, Link3, Link4])
-        self.ReachAlpha5.q = [0, 1.5707 - self.ThetaA, 0 - self.ThetaA , 0 + pi/2, 0]
+        self.ReachAlpha5.q = [0 + pi, 1.5707 - self.ThetaA, 0 - self.ThetaA , 0 + pi/2, 0]
+        self.Origin = self.ReachAlpha5.q 
+        print(self.ReachAlpha5.fkine(self.Origin))
     
-    def CalculatePosition(self):
+    def CalculateandMove(self):
+        self.steps = 100
         for self.index in range(len(self.coordinates)):
-            if pow(sqrt(pow(self.coordinates[self.index,0],2)+pow(self.coordinates[self.index,1],2))- self.ReachAlpha5.a[0],2)+pow(self.coordinates[self.index,2]-self.ReachAlpha5.d[0],2) <= pow(self.ReachAlpha5.a[1]+sqrt(pow(self.ReachAlpha5.d[3],2)+pow(self.ReachAlpha5.a[2],2)),2) and pow(sqrt(pow(self.coordinates[self.index,0],2)+ pow(self.coordinates[self.index,1],2)) - self.ReachAlpha5.a[0],2)+pow(self.coordinates[self.index,2]-self.ReachAlpha5.d[0],2) >= (pow(39.94 + self.ReachAlpha5.a[2],2)+pow(145.3+self.ReachAlpha5.d[3],2)):
-                print('true')
+            self.x = self.coordinates[self.index,0]
+            self.y = self.coordinates[self.index,1]
+            self.z = self.coordinates[self.index,2]
+            self.outer_limits = pow(sqrt(pow(self.x,2)+pow(self.y,2)) - self.ReachAlpha5.a[0],2) + pow(self.z - self.ReachAlpha5.d[0],2) <= pow(self.ReachAlpha5.a[1]  + sqrt(pow(self.ReachAlpha5.d[3],2)+pow(self.ReachAlpha5.a[2],2)),2)
+            self.inner_limits = pow(sqrt(pow(self.x,2)+pow(self.y,2)) - self.ReachAlpha5.a[0],2) + pow(self.z - self.ReachAlpha5.d[0],2) >= (pow(0.03994 + self.ReachAlpha5.a[2],2) + pow(0.1453 + self.ReachAlpha5.d[3],2))
+            self.inner_lower_limits = pow(sqrt(pow(self.x,2)+pow(self.y,2)) - self.ReachAlpha5.a[0],2) + pow(self.z - self.ReachAlpha5.d[0] + 0.1453,2) >= pow(-self.ReachAlpha5.d[3],2)
+            if self.outer_limits and self.inner_limits and self.inner_lower_limits :
+                print('Reachable Position: ', self.coordinates[self.index])
+                T1 = transl(self.coordinates[self.index])
+                self.qdestination = self.ReachAlpha5.ikine_LM(T1,q0=self.ReachAlpha5.q).q
+                self.trajectory = jtraj(self.ReachAlpha5.q, self.qdestination,self.steps).q
+                fig = plt.figure(1)
+                fig = self.ReachAlpha5.plot(self.ReachAlpha5.q,fig=fig)
+                ax = plt.gca()
+                for self.q in self.trajectory:
+                    self.desired_position = [degrees(self.q[4]), self.q[3], self.q[2], self.q[1], self.q[0]]
+                    self.ReachAlpha5.q = self.q
+                    fig.step(0.05)
+                    
+                    packets = b''
+                    #for index, position in enumerate(self.desired_position):
+                        #device_id = index + 1
+                        #packets += BPLProtocol.encode_packet(device_id, PacketID.POSITION, BPLProtocol.encode_floats([position]))
+                        #self.serial_port.write(packets)
+                
+                print(self.ReachAlpha5.fkine(self.ReachAlpha5.q))
 
+                self.trajectoryback= jtraj(self.ReachAlpha5.q, self.Origin,self.steps).q
+                for self.q in self.trajectoryback:
+                    self.desired_position = [degrees(self.q[4]), self.q[3], self.q[2], self.q[1], self.q[0]]
+                    self.ReachAlpha5.q = self.q
+                    fig.step(0.05)
+                    
+                    packets = b''
+                    #for index, position in enumerate(self.desired_position):
+                        #device_id = index + 1
+                        #packets += BPLProtocol.encode_packet(device_id, PacketID.POSITION, BPLProtocol.encode_floats([position]))
+                        #self.serial_port.write(packets)
+                print(self.ReachAlpha5.fkine(self.ReachAlpha5.q))
             else:
-                print('false')
+                print('Unreachable Position: ', self.coordinates[self.index])
+                continue
+
+if __name__ == '__main__':
+    print('hello world')
+    Coordinates = [[0.0,0.0,0.0],[0.0,0.5,0.4],[-0.019, -0.138, 0.213]]
+    Kinematics(Coordinates=Coordinates, COMPORT='COM3')
--- Virtual_WorkSpace/RobotTest.py
+++ Virtual_WorkSpace/RobotTest.py
@@ -18,12 +18,12 @@ import serial
 desired_positions = [0.0, 0.0, 0.0, 1.5707, 0.0] # A = grabber open/close (Link 4) B = Link 3 C = Link 2 B = Link 1 C = Link 0 
 ThetaA = atan(145.3/40)
 Link0 = DHLink(d= 0.0462, a= 0.020, alpha= pi/2, qlim= [radians(0),radians(350)]) # Base Link
-Link1 = DHLink(d= 0, a= 0.15071, alpha= pi, qlim= [-radians(0),radians(200)]) 
+Link1 = DHLink(d= 0, a= 0.15071, alpha= pi, qlim= [radians(90),radians(290)]) 
 Link2 = DHLink(d= 0, a= 0.020, alpha= -pi/2, qlim= [-radians(0),radians(200)])
-Link3 = DHLink(d= -0.180, a= 0, alpha= pi/2, qlim= [radians(-0),radians(350)])
+Link3 = DHLink(d= -0.180, a= 0, alpha= pi/2, qlim= [radians(0),radians(350)])
 Link4 = DHLink(d= 0, a= 0, alpha= 0, qlim= [-radians(0), radians(90)]) # Grabber
-
-
+# all links 0 - limit besides link 1
+print('ThetaA = ', ThetaA)
 workspace = [-0.4, 0.4, -0.4, 0.4, -0.4, 0.4]                   # Set the size of the workspace
 ReachAlpha5 = DHRobot([Link0 , Link1, Link2, Link3, Link4])
 ReachAlpha5.q = [0, 1.5707 - ThetaA, 0 - ThetaA , 0 + pi/2, 0]     #starting position                                                  # Create a vector of initial joint angles
@@ -37,6 +37,7 @@ Actual = ReachAlpha5.fkine(q1)
 Distance = norm(Desired - Actual.t)
 print('Desired Fkine = ', Desired, 'Actual Fkine = ', Actual.t)
 print('Distance between = ', Distance)
+
 # Desired position from end effector to base A -> E
 desired_positions = [degrees(q1[4]), q1[3], q1[2],q1[1],q1[0]]
 #packets = b''
@@ -44,10 +45,17 @@ desired_positions = [degrees(q1[4]), q1[3], q1[2],q1[1],q1[0]]
     #device_id = index + 1
     #packets += BPLProtocol.encode_packet(device_id, PacketID.POSITION, BPLProtocol.encode_floats([position]))
     #serial_port.write(packets)
-outer_limits = pow((ReachAlpha5.a[1]+sqrt(ReachAlpha5.d[3]*ReachAlpha5.d[3]+ReachAlpha5.a[2]*ReachAlpha5.a[2])),2)
-define = np.array([[0, 0, 0],[0,1,0],[0,1,2]])
-print(define[1])
-if pow(sqrt(pow(define[0],2)+pow(define[1],2))- ReachAlpha5.a[0],2)+pow(define[2]-ReachAlpha5.d[0],2) <= pow(ReachAlpha5.a[1]+sqrt(pow(ReachAlpha5.d[3],2)+pow(ReachAlpha5.a[2],2)),2) and pow(sqrt(pow(define[0],2)+ pow(define[1],2)) - ReachAlpha5.a[0],2)+pow(define[2]-ReachAlpha5.d[0],2) >= (pow(39.94 + ReachAlpha5.a[2],2)+pow(145.3+ReachAlpha5.d[3],2)):
+coordiantes = [0,2,1]
+define = np.array(coordiantes)
+x = define[0]
+y = define[1]
+z = define[2]
+for index in range(len(define)):
+    print(define[index])
+outer_limits = pow(sqrt(pow(x,2)+pow(y,2)) - ReachAlpha5.a[0] ,2) + pow(z - ReachAlpha5.d[0],2) <= pow(ReachAlpha5.a[1]  + sqrt(pow(ReachAlpha5.d[3],2)+pow(ReachAlpha5.a[2],2)),2)
+inner_limits = pow(sqrt(pow(x,2)+pow(y,2)) - ReachAlpha5.a[0],2) + pow(z - ReachAlpha5.d[0],2) >= (pow(0.03994 + ReachAlpha5.a[2],2) + pow(0.1453 + ReachAlpha5.d[3],2))
+
+if inner_limits and outer_limits:
     print('Winner')
 
 else:
