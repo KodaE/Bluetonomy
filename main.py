0260a0b697f852bb2223ad7f9521e8f1cbaeca78
khacminhle <100dayscode.minh@gmail.com>
2023-10-09 14:17:23 +1100

    impove RA5 speed

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from Reach_Kinematics import Kinematics
 import random
+import time
 
+RA_km = Kinematics(COMPORT="COM6")
 
-RA_km = Kinematics(COMPORT="COM4")
 while True:
-
     x = random.uniform(0, 0.3)
     y = random.uniform(0, 0.3)
     z = random.uniform(0, 0.3)
     co = [[x,y,z]]
     print(co)
     RA_km.CalculateandMove(coordinates=co)
+    
     
 
 
