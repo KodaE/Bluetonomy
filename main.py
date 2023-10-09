6d0368c6a5cad403a1d01f4a8d522197840f843f
khacminhle <100dayscode.minh@gmail.com>
2023-10-09 14:23:11 +1100

    Update to Main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import Reach_Control
 
-RA5_control = Reach_Control.Reach_Control_Class(serial_port_name="COM4")
-
-RA5_control.move_arm_to_pos([0.0, 0.0, 0.0, 1.5707, 0.0])
+RA5_control = Reach_Control.Reach_Control_Class(serial_port_name="COM6")
 
 
-print("Hello World")
+
+base_id = RA5_control.base_id_feedback_data()
+
+print(base_id)
 
