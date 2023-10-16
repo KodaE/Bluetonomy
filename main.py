6e71aba426451ee309352d110914a9f747b14172
dugarte4 <daniel.m.ugarte4@gmail.com>
2023-10-16 11:33:43 +1100

    Updating Main

--- main.py
+++ main.py
@@ -1,9 +1,60 @@
-import Reach_Control
+from Reach_Kinematics import Kinematics
+import random
+import time 
+import keyboard
+import threading
 
-RA5_control = Reach_Control.Reach_Control_Class(serial_port_name="COM4")
+HOME = [0.0, 0.0, 0.0, 1.5207, 0.0]
 
-RA5_control.move_arm_to_pos([0.0, 0.0, 0.0, 1.5707, 0.0])
+RA_km = Kinematics(COMPORT="COM6")
+# RA_km.move_arm_to_pos(HOME)
+
+timeout = time.time() + 5
 
 
-print("Hello World")
+e_stop_event = threading.Event()
+
+
+def generate_valid_coord():
+    x = random.uniform(0, 0.3)
+    y = random.uniform(0, 0.3)
+    z = random.uniform(0, 0.3)
+    co = [[x,y,z]]
+    return co
+
+def run_function():
+    while not e_stop_event.is_set():
+        co = generate_valid_coord()
+        print(RA_km.stop_flag)
+        RA_km.CalculateandMove(coordinates=co)
+        print("\n")
+
+
+def e_stop():
+    while True:
+        print("e-stop running")
+        if keyboard.is_pressed("a"):
+            RA_km.send_disable_comms()
+            RA_km.stop_arm_movement()
+            e_stop_event.set()
+            RA_km.stop_flag = True
+            break
+
+
+t1 = threading.Thread(target=e_stop)
+t2 = threading.Thread(target=run_function)
+
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+
+
+
+
+
+
+
+
+
 
