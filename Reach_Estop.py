815293747a7ace99cf34b8afbd1c8e6b8e5a7f5f
dugarte4 <daniel.m.ugarte4@gmail.com>
2023-10-16 11:27:51 +1100

    Updating Estop Branch

--- Reach_Estop.py
+++ Reach_Estop.py
@@ -1,6 +1,31 @@
+import time
+import threading
+
+
 class Reach_Estop_Class:
     def __init__(self) -> None:
+        self.time = time.time()
+        self.flag = False
+        
+    
+
+    def run(self): 
+        while True:
+            self.time = time.time()
+            
+        
+
+    def checktorque(self): #Check for the reading of torque during, will need to time it so that when checking for tolerance it isnt using the same path
+        self.mutex.acquire()
+
+        self.mutex.release()
+
+    def checktemp(self):
         pass
 
+
+
+
 if __name__ == "__main__":
-    pass
+    kin = Reach_Estop_Class()
+    kin.run()
