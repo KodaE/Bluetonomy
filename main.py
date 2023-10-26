c7e96689f47b6ade555b5e95718f45dde12b9bed
khacminhle <100dayscode.minh@gmail.com>
2023-10-26 13:00:28 +1100

    stable version minus -y movement and arm reengagement after disabling

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import time
 import keyboard
 import threading
 import time
-import datetime
-import random
+
 
 
 
@@ -17,12 +16,27 @@ coord = [[100, 100, 100]]
 program_is_on = True
 coordinates = []
 data_log_file_title = time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime(time.time()))
-
+e_stop_flag = False
 
 RA_km = Kinematics(COMPORT="COM6")
 time.sleep(0.001)
 RA_km.send_standby_comms()
-# RA_km.move_arm_to_pos(HOME)
+RA_km.move_arm_to_pos(HOME)
+
+#-------------------------------------------- Program Title -----------------------------------------------#
+
+
+print('''______   ___   _____   _____                _                _  ______                                          
+| ___ \ / _ \ |  ___| /  __ \              | |              | | | ___ \                                         
+| |_/ // /_\ \|___ \  | /  \/  ___   _ __  | |_  _ __  ___  | | | |_/ /_ __  ___    __ _  _ __  __ _  _ __ ___  
+|    / |  _  |    \ \ | |     / _ \ | '_ \ | __|| '__|/ _ \ | | |  __/| '__|/ _ \  / _` || '__|/ _` || '_ ` _ \ 
+| |\ \ | | | |/\__/ / | \__/\| (_) || | | || |_ | |  | (_) || | | |   | |  | (_) || (_| || |  | (_| || | | | | |
+\_| \_|\_| |_/\____/   \____/ \___/ |_| |_| \__||_|   \___/ |_| \_|   |_|   \___/  \__, ||_|   \__,_||_| |_| |_|
+                                                                                    __/ |                       
+                                                                                   |___/                        ''')
+
+
+#----------------------------------------------------------------------------------------------------------#
 
 
 def run_data_log_program():
@@ -44,6 +58,7 @@ def run_main_program():
             user_action = input("Please select following button to perform action. Press a to coordinates, b to submit coordinate and c to terminate program, d to home arm: ")
             
             if user_action == "d":
+            
                  RA_km.send_standby_comms()
                  RA_km.move_arm_to_pos(HOME)
             
@@ -95,12 +110,14 @@ def run_main_program():
                                 print(f"RA 5 is not available now - {e}")
                                 user_action = input("Press a to home, b to restart program and c to terminate program: ")
                                 if user_action == "a":
+                                
                                     RA_km.send_standby_comms()
                                     RA_km.move_arm_to_pos(HOME)
 
                                 elif user_action == "b":
-                                     RA_km.send_standby_comms()
-                                     pass
+                            
+                                    RA_km.send_standby_comms()
+                                    
                                 elif user_action == "c":
                                      program_is_on = False
                                 
@@ -122,8 +139,8 @@ def e_stop():
         voltage = RA_km.base_id_feedback_data()["voltage"][0]
         temp = RA_km.base_id_feedback_data()["temp"][0]
     except:
-        voltage = "Not available"
-        temp = "Not available"
+        voltage is None
+        temp is None
 
    
     # temp = RA_km.base_id_feedback_data()["temp"]
@@ -131,30 +148,30 @@ def e_stop():
 
     while program_is_on:
         current_time = time.time()
-        if current_time - start >= 10:
+        if current_time - start >= 20:
             readable_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(current_time))
             time.sleep(2)
-            print("\n#---------------------- System Status --------------------------#")
+            print("\n#---------------------- System Status ---------------------------#")
             print(f"\nE - Stop is alive {readable_time}")
             print(f"The current voltage is: {voltage}")
             print(f"The current temperature is {temp}\n")
             print("#-----------------------------------------------------------------#\n")
             start = current_time
-
-        if keyboard.is_pressed("z") or temp > 45 or voltage > 30:
+    
+        if  keyboard.is_pressed("z") or temp > 100 or voltage > 50:
             try:
                 print("\ne - stop pressed\n")
+               
                 RA_km.send_disable_comms()
+                time.sleep(4)
             except:
-                 pass
+                pass
        
               
-                               
-        
-
-t2 = threading.Thread(target=run_main_program)
 t1 = threading.Thread(target=e_stop)
+t2 = threading.Thread(target=run_main_program)
 t3 = threading.Thread(target=run_data_log_program)
+
 
 
 
@@ -162,13 +179,11 @@ t1.start()
 t2.start()
 t3.start()
 
+
 t1.join()
 t2.join()
 t3.join()
-
-
-
-            
+     
         
             
         
